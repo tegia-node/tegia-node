8bc183eaf8850fc3e664652ce9824fbc47e3cf04
Igor Goryachev <igor@tegia.ru>
2024-02-07 12:46:24 +0300

    Добавлена настройка xml2json и mysql

--- install.sh
+++ install.sh
@@ -20,6 +20,12 @@ echo "TEGIA NODE: ${GREEN} DEPENDENCES ${RESET}"
 echo "------------------------------------------------------------"
 echo " "
 
+mkdir -p ${root_folder}/tegia-node/build
+mkdir -p ${root_folder}/vendors
+mkdir -p ${root_folder}/configurations
+mkdir -p ${root_folder}/ui
+
+
 #
 # ENV
 #
@@ -68,11 +74,24 @@ fi
 ${root_folder}/vcpkg/vcpkg install nlohmann-json json-schema-validator fmt vincentlaucsb-csv-parser cpp-jwt
 
 #
+# xml2json
+#
+
+if ! [ -d  ${root_folder}/vendors/xml2json/ ]
+then
+	cd ${root_folder}/vendors
+	git clone https://github.com/Cheedoong/xml2json
+fi
+
+# TODO: переделать на более правильный вариант
+sudo ln -fs {root_folder}/vendors/xml2json /usr/include/xml2json
+
+
+#
 # CONFIGURE
 #
 
 cd ${root_folder}/tegia-node
-mkdir ${root_folder}/tegia-node/build
 cmake -B build/ -S . -DCMAKE_TOOLCHAIN_FILE=${root_folder}/vcpkg/scripts/buildsystems/vcpkg.cmake
 
 echo " "
@@ -90,16 +109,30 @@ echo "TEGIA NODE: ${GREEN} INSTALL ${RESET}"
 echo "------------------------------------------------------------"
 echo " "
 
-
-exit 0
-
-# ----------------------------------------------------------------------------------------
 #
-# Настраиваем подключение к MySQL
+# MySQL
 #
-# ----------------------------------------------------------------------------------------
 
+ismysql80="$(dpkg --get-selections | grep mysql-server-8.0)"
+if [[ "${#ismysql80}" == 0 ]]; then
 
+	mkdir -p $tegia_folder/vendors/mysql
+	cd $tegia_folder/vendors/mysql
+	wget -N https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
+	sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.15-1_all.deb    
+
+	sudo apt-get update -y
+	sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+	sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-client
+
+	echo -e "${_OK_}MySQL success installed"
+else
+	echo -e "${_OK_}MySQL is already installed"
+fi
+
+#
+# TEGIA USER FOR MySQL
+#
 
 mysql_debian_password()
 {
@@ -112,6 +145,74 @@ mysql_debian_password()
     passwd="${passwd:3:$len}"
     echo $passwd
 }
+
+read -rp "${YELLOW}Укажите имя для пользователя, который будет использоваться для подключения к MySQL:${RESET} [tegia_user]: " mysql_user
+if [[ -z "$mysql_user" ]]; then
+	mysql_user="tegia_user"
+fi
+
+while [[ -z "$mysql_password" ]]; do
+	read -srp "${YELLOW}Укажите пароль, который будет использоваться для подключения к MySQL:${RESET} " mysql_password
+	if [[ -z "$mysql_password" ]]; then
+		echo -e "\n${_ERR_}Пароль не может быть пустым"
+	else
+		break
+	fi
+done
+echo " "
+
+mysql_host='localhost'
+mysql_port='3306'
+
+#
+# CREATE TEGIA USER
+#
+
+export MYSQL_PWD=$(mysql_debian_password)
+
+iffinduser="$(mysql -u debian-sys-maint --execute="SELECT host,user FROM mysql.user WHERE host = '$mysql_host' AND user = '$mysql_user';")"
+if [[ "${#iffinduser}" != 0 ]]; then
+
+	#
+	# Удалить пользователя
+	#
+	
+	mysql -u debian-sys-maint --port=$mysql_port --execute="DROP USER '$mysql_user'@'$mysql_host';"
+fi
+
+mysql -u debian-sys-maint --port=$mysql_port << EOF
+CREATE USER '$mysql_user'@'$mysql_host' IDENTIFIED BY '$mysql_password';
+GRANT ALL PRIVILEGES ON *.* TO '$mysql_user'@'$mysql_host';
+FLUSH PRIVILEGES;
+EOF
+
+echo -e "${_OK_}tegia user '${mysql_user}' is created on MySQL"
+
+#
+# SAVE tegia.cnf FILE
+#
+
+tee ${root_folder}/tegia.cnf << EOF > /dev/null
+[mysql]
+host=$mysql_host
+port=$mysql_port
+user=$mysql_user
+password=$mysql_password
+EOF
+
+echo "${_OK_}file '${root_folder}/tegia.cnf' is saved"
+
+
+
+exit 0
+
+# ----------------------------------------------------------------------------------------
+#
+# Настраиваем подключение к MySQL
+#
+# ----------------------------------------------------------------------------------------
+
+
 
 
 mysql_install()
@@ -135,79 +236,12 @@ mysql_install()
 	fi
 
 	#echo "Укажите mysql user"
-	read -rp "${YELLOW}Укажите mysql user:${RESET} [tegia_user]: " mysql_user
-	if [[ -z "$mysql_user" ]]; then
-		mysql_user="tegia_user"
-	fi
-
-	while [[ -z "$mysql_password" ]]; do
-		read -srp "${YELLOW}Укажите пароль для подключения к MySQL:${RESET} " mysql_password
-		if [[ -z "$mysql_password" ]]; then
-			echo -e "\n${_ERR_}Пароль не может быть пустым"
-		else
-			break
-		fi
-	done
-	echo " "
 
     #
     # install mysql local
     #
 
-    ismysql80="$(dpkg --get-selections | grep mysql-server-8.0)"
-    if [[ "${#ismysql80}" == 0 ]]; then
 
-        mkdir -p $tegia_folder/vendors/mysql
-        cd $tegia_folder/vendors/mysql
-        wget -N https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
-        sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.15-1_all.deb    
-
-        sudo apt-get update -y
-        sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
-        sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-client
-
-        echo -e "${_OK_}MySQL success installed"
-    else
-        echo -e "${_OK_}MySQL is already installed"
-    fi
-
-    #
-    # CREATE TEGIA USER
-    #
-
-    export MYSQL_PWD=$(mysql_debian_password)
-
-    iffinduser="$(mysql -u debian-sys-maint --execute="SELECT host,user FROM mysql.user WHERE host = '$mysql_host' AND user = 'tegia_user';")"
-    if [[ "${#iffinduser}" != 0 ]]; then
-
-        #
-        # Удалить пользователя
-        #
-        
-        mysql -u debian-sys-maint --port=$mysql_port --execute="DROP USER 'tegia_user'@'$mysql_host';"
-    fi
-
-    mysql -u debian-sys-maint --port=$mysql_port << EOF
-    CREATE USER 'tegia_user'@'$mysql_host' IDENTIFIED BY '$mysql_password';
-    GRANT ALL PRIVILEGES ON *.* TO 'tegia_user'@'$mysql_host';
-    FLUSH PRIVILEGES;
-EOF
-
-    echo -e "${_OK_}tegia_user is created on MySQL"
-
-	#
-	# SAVE tegia.cnf FILE
-	#
-
-	tee ../tegia.cnf << EOF > /dev/null
-[mysql]
-host=$mysql_host
-port=$mysql_port
-user=$mysql_user
-password=$mysql_password
-EOF
-
-	echo "${_OK_}file '$tegia_folder/tegia.cnf' is saved"
 
 }
 
@@ -230,56 +264,8 @@ select opt in "${options[@]}"; do
     esac
 done
 
-# ----------------------------------------------------------------------------------------
-#
-# Устанавливаем зависимости
-#
-# ----------------------------------------------------------------------------------------
 
-sudo apt install -y mc screen jq zip
-sudo apt install -y python build-essential default-libmysqlclient-dev libtool m4 automake uuid-dev libxml2-dev
-sudo apt install -y libcurl4-openssl-dev libssl-dev
-sudo apt install -y cmake zlibc
-sudo apt install -y libbz2-dev libzip-dev unzip
-sudo apt install -y pkg-config
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt install -y g++-11 gcc-11
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
-
-# ----------------------------------------------------------------------------------------
-#
-# Настраиваем директории
-#
-# ----------------------------------------------------------------------------------------
-
-mkdir -p $tegia_folder/vendors
-mkdir -p $tegia_folder/configurations
-mkdir -p $tegia_folder/ui
-
-cd $tegia_folder
-ln -s $tegia_folder/tegia-node/include $tegia_folder/tegia-node/tegia
-
-# ----------------------------------------------------------------------------------------
-#
-# Загружаем и настраиваем используемые библиотеки
-#
-# ----------------------------------------------------------------------------------------
-
-cd $tegia_folder/vendors
-git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
-
-sudo ./bootstrap-vcpkg.sh -disableMetrics
-sudo ./vcpkg install nlohmann-json json-schema-validator fmt vincentlaucsb-csv-parser xpack cpp-jwt
-
-#
-# xml2json
-#
-
-cd $tegia_folder/vendors
-git clone https://github.com/Cheedoong/xml2json
-sudo ln -fs $tegia_folder/vendors/xml2json /usr/include/xml2json
 
 # ----------------------------------------------------------------------------------------
 #
